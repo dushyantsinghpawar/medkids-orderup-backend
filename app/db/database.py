149196c7ff418bb7e65a9112e0102fbdcd78731a
Dushyant Singh Pawar <dushyantsinghpawar@gmail.com>
2026-02-23 16:39:28 -0500

    Fix: read DATABASE_URL from environment variable

--- app/db/database.py
+++ app/db/database.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql://medkids:medkids@localhost:5432/medkids"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://medkids:medkids@db:5432/medkids")
 
 engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 # Import models so Alembic can detect them
